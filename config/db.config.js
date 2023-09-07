cc8251f30318bafe9ea2d6e8981434ba0eefaf11
steve <stephen@stephenmucheru79@gmail.com>
2023-09-07 10:09:07 +0300

    all Files

---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "phpuser",
+    PASSWORD: "V!v@dm!n?1",
+    DB: "tutorial",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
